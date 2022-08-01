7f8b8279675bffacc89fb3e8dc97bddcdda7f9e3
pooyaPera <pooya.pera@gmail.com>
2022-08-01 13:22:39 +0200

    commenting

--- src/main.java
+++ src/main.java
@@ -14,16 +14,17 @@ public class main {
 
         FileOutputStream output = new FileOutputStream("text.txt");
 
-        String statment = "Hello world";
-        byte[] statmentbyte = statment.getBytes(StandardCharsets.UTF_8);
-        output.write(statmentbyte);
-        output.close();
+        String statment = "Hello world"; //Creating an statememt to work out
+        byte[] statmentbyte = statment.getBytes(StandardCharsets.UTF_8);//parsing it into bytes
+        output.write(statmentbyte);//writng byts to the file
+        output.close();//closing the file
 
-        String path = "C:\\Users\\Home\\IdeaProjects\\playingWithFiles\\text.txt";
-        FileInputStream input = new FileInputStream(path);
+        String path = "C:\\Users\\Home\\IdeaProjects\\playingWithFiles\\text.txt";//finding the file
+        FileInputStream input = new FileInputStream(path);//inputting it
+        //System.out.println((int)input.read());//Outputs the Byte value of the first letter of the sentence in this case H with value of 72
         int  i= 0;
-        while ((i= input.read())!= -1){
-            System.out.println((char)i);}
+        while ((i= input.read())!= -1) {
+            System.out.print((char)i);}//printing it out
 
 
 
